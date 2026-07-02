67e080cdfe5789e45ced5aa62bec242a85cb3541
Kenneth Tablang <kennethreytablang@gmail.com>
2026-07-02 12:02:33 +0800

    Add columns for defense outcome, revision, and urgency
    Added DefenseOutcome, RevisionLevel, and RequiresReDefense to CapstoneGroups, and Urgency to SystemFeatures. Updated migration and model snapshot to reflect schema changes. Columns have default values and are removed on rollback.

--- THESISMATESystem.Server/Migrations/20260629003858_AddDefenseOutcomeAndUrgency.cs
+++ THESISMATESystem.Server/Migrations/20260629003858_AddDefenseOutcomeAndUrgency.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,13 +10,53 @@ namespace THESISMATESystem.Server.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<int>(
+                name: "DefenseOutcome",
+                table: "CapstoneGroups",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
 
+            migrationBuilder.AddColumn<int>(
+                name: "RevisionLevel",
+                table: "CapstoneGroups",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequiresReDefense",
+                table: "CapstoneGroups",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Urgency",
+                table: "SystemFeatures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "DefenseOutcome",
+                table: "CapstoneGroups");
 
+            migrationBuilder.DropColumn(
+                name: "RevisionLevel",
+                table: "CapstoneGroups");
+
+            migrationBuilder.DropColumn(
+                name: "RequiresReDefense",
+                table: "CapstoneGroups");
+
+            migrationBuilder.DropColumn(
+                name: "Urgency",
+                table: "SystemFeatures");
         }
     }
 }
